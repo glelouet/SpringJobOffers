61788eaf677af363d045ce2c73efcfc9022db793
glelouet <n@mo>
2023-06-26 14:53:16 +0200

    import from https://start.spring.io/

---
+++ src/test/java/fr/lelouet/springJobOffers/SpringJobOffersApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.lelouet.springJobOffers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJobOffersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
